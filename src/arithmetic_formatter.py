a3b3ff1f4013adf65b0a45b43c42886c78135c90
Aymen Sellami <Aymen.Sellami@hhu.de>
2023-07-13 09:25:23 +0200

    Use concise syntax for string formatting

--- src/arithmetic_formatter.py
+++ src/arithmetic_formatter.py
@@ -32,25 +32,21 @@ def arithmetic_arranger(problems, evaluate=False) -> str:
         four_spaces = " " * 4
         # Adjust the spacing based on which operand is longer
         if len(first_operand) > len(second_operand):
-            first_line += "  {}".format(first_operand) + four_spaces
-            second_line += "{}".format(operator) + " " * (len(first_operand) - len(second_operand) + 1) + "{}".format(
-                second_operand) + four_spaces
+            first_line += f"  {first_operand}" + four_spaces
+            second_line += f"{operator}" + " " * (len(first_operand) - len(second_operand) + 1) + f"{second_operand}" + four_spaces
             third_line += "-" * (len(first_operand) + 2) + "    "
             if len(first_operand) >= len(str(result)):
-                fourth_line += " " * (2 + len(first_operand) - len(str(result))) + "{}".format(
-                    result) + four_spaces
+                fourth_line += " " * (2 + len(first_operand) - len(str(result))) + f"{result}" + four_spaces
             else:
-                fourth_line += " {}".format(result) + four_spaces
+                fourth_line += f" {result}" + four_spaces
         else:
-            first_line += " " * (len(second_operand) - len(first_operand) + 2) + "{}".format(
-                first_operand) + four_spaces
-            second_line += "{} {}".format(operator, second_operand) + four_spaces
+            first_line += " " * (len(second_operand) - len(first_operand) + 2) + f"{first_operand}" + four_spaces
+            second_line += f"{operator} {second_operand}" + four_spaces
             third_line += "-" * (len(second_operand) + 2) + four_spaces
             if len(second_operand) >= len(str(result)):
-                fourth_line += " " * (2 + len(second_operand) - len(str(result))) + "{}".format(
-                    result) + four_spaces
+                fourth_line += " " * (2 + len(second_operand) - len(str(result))) + f"{result}" + four_spaces
             else:
-                fourth_line += " {}".format(result) + four_spaces
+                fourth_line += f" {result}" + four_spaces
         # Strip the four spaces from the last problem and add new lines
         if problem == problems[len(problems) - 1]:
             first_line = first_line.rstrip() + "\n"
